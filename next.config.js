6f4cdb1f7b455b45dabb348dc0f3b22e1c7d2ee1
quandat10 <datcckkdat@gmail.com>
2023-04-04 07:18:56 +0700

    fix:webpack-warning

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          fs: false,
+        },
+      }
+    }
+
+    config.module = {
+      ...config.module,
+      exprContextCritical: false,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
