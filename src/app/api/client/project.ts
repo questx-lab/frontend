0a2d61c6871dcaeb64aa96d3ef6d039a210ac73c
quandat10 <datcckkdat@gmail.com>
2023-04-05 11:42:05 +0700

    remove log

--- src/app/api/client/project.ts
+++ src/app/api/client/project.ts
@@ -4,7 +4,6 @@ import { ReqNewProject } from '@/types/project.type'
 import { api } from '../config/api'
 
 export const NewProjectApi = async (body: ReqNewProject) => {
-  console.log(body)
   const rs = await api.post(
     EnvVariables.NEXT_PUBLIC_API_URL + '/createProject',
     body
