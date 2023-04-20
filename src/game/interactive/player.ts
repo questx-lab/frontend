037bff40ac2532a993ba0cf34b078b6f3232e6a9
quandat10 <datcckkdat@gmail.com>
2023-04-21 01:47:23 +0700

    feat:game engine

---
+++ src/game/interactive/player.ts
@@ -0,0 +1,131 @@
+import Phaser from 'phaser'
+
+export class Player extends Phaser.GameObjects.Sprite {
+  speed: number
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    texture: string | Phaser.Textures.Texture,
+    frame: string | number | undefined
+  ) {
+    super(scene, x, y, texture, frame)
+
+    // Add the sprite and the physics body to the scene
+    scene.add.existing(this)
+    scene.physics.add.existing(this)
+
+    // Set the depth and the size
+    this.setDepth(5)
+    ;(this.body as any).setSize(26, 41)
+
+    // Create animations
+    this.createAnims(scene)
+
+    // Speed of movement
+    this.speed = 175
+  }
+
+  createAnims(scene: Phaser.Scene) {
+    // Create the player's walking animations from the texture atlas
+    const anims = scene.anims
+    anims.create({
+      key: 'ariel-left-walk',
+      frames: anims.generateFrameNames('atlas', {
+        prefix: 'ariel-left-walk.',
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+    anims.create({
+      key: 'ariel-right-walk',
+      frames: anims.generateFrameNames('atlas', {
+        prefix: 'ariel-right-walk.',
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+    anims.create({
+      key: 'ariel-front-walk',
+      frames: anims.generateFrameNames('atlas', {
+        prefix: 'ariel-front-walk.',
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+    anims.create({
+      key: 'ariel-back-walk',
+      frames: anims.generateFrameNames('atlas', {
+        prefix: 'ariel-back-walk.',
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+    anims.create({
+      key: 'ariel-wave',
+      frames: anims.generateFrameNames('atlas', {
+        prefix: 'ariel-wave.',
+        start: 0,
+        end: 4,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+  }
+
+  update(moveleft: any, moveright: any, moveup: any, movedown: any) {
+    const prevVelocity = (this.body as any).velocity.clone() // Stop any previous movement from the last frame
+    ;(this.body as any).setVelocity(0)
+
+    // Update the animation and give left/right animations precedence over up/down animations in diagonal movement
+    if (moveleft) {
+      ;(this.body as any).setVelocityX(-this.speed)
+      this.anims.play('ariel-left-walk', true)
+    } else if (moveright) {
+      ;(this.body as any).setVelocityX(this.speed)
+      this.anims.play('ariel-right-walk', true)
+    }
+    if (moveup) {
+      ;(this.body as any).setVelocityY(-this.speed)
+      if (!(moveleft || moveright)) {
+        // When moving diagonally display the left / right animation
+        this.anims.play('ariel-back-walk', true)
+      }
+    } else if (movedown) {
+      ;(this.body as any).setVelocityY(this.speed)
+      if (!(moveleft || moveright)) {
+        // When moving diagonally display the left / right animation
+        this.anims.play('ariel-front-walk', true)
+      }
+    }
+
+    ;(this.body as any).velocity.normalize().scale(this.speed)
+
+    if (
+      !(moveleft || moveright || moveup || movedown) &&
+      !(
+        this.anims.currentAnim == null ||
+        this.anims.currentAnim.key === 'ariel-wave'
+      )
+    ) {
+      this.anims.stop()
+      if (prevVelocity.x < 0) this.setTexture('atlas', 'ariel-left')
+      else if (prevVelocity.x > 0) this.setTexture('atlas', 'ariel-right')
+      else if (prevVelocity.y < 0) this.setTexture('atlas', 'ariel-back')
+      else if (prevVelocity.y > 0) this.setTexture('atlas', 'ariel-front')
+    }
+  }
+}
--- src/game/scenes/base.ts
+++ src/game/scenes/base.ts
@@ -13,10 +13,8 @@ export class BaseScene extends Phaser.Scene {
   // --------------------------------------------------------------------------------------------------
   // CREATE
   create(tilemapKey: string) {
-    // ----------------
-    // MAP AND TILESET
+    console.log('hello create')
     this.map = this.make.tilemap({ key: tilemapKey })
-
     const tileset = this.map.addTilesetImage(
       'tileset',
       'TilesetImage',
@@ -30,9 +28,74 @@ export class BaseScene extends Phaser.Scene {
     this.map.createLayer('Ground2', tileset, 0, 0)
     this.map.createLayer('Collision1', tileset, 0, 0)
     this.map.createLayer('Collision2', tileset, 0, 0)
-    this.map.createLayer('Above', tileset, 0, 0).setDepth(10)
+    this.map.createLayer('Above', tileset, 0, 0).setDepth(10) // To have the "Above" layer sit on top of the player, we give it a depth.
     this.LayerToCollide = this.map.createLayer('CollisionLayer', tileset, 0, 0)
-    this.LayerToCollide.setVisible(false)
+    this.LayerToCollide.setVisible(false) // Comment out this line if you wish to see which objects the player will collide with
+
+    // ----------------
+    // PLAYER
+    const spawnPoint = this.map.findObject(
+      'Objects',
+      (obj: { name: string }) => obj.name === 'Spawn Point'
+    )
+
+    // Create the player and the player animations (see player.js)
+    this.player = (this.add as any).player(
+      spawnPoint.x,
+      spawnPoint.y,
+      'atlas',
+      'ariel-front'
+    )
+
+    // CAMERA AND CURSORS
+    const camera = this.cameras.main
+    camera.startFollow(this.player)
+    camera.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
+    this.cursors = this.input.keyboard.createCursorKeys()
+    this.wasd = {
+      w: this.input.keyboard.addKey('W'),
+      a: this.input.keyboard.addKey('A'),
+      s: this.input.keyboard.addKey('S'),
+      d: this.input.keyboard.addKey('D'),
+    }
+
+    // Camera resize behavior
+    this.scale.on('resize', this.resize, this)
+
+    // ----------------
+    // INTERACTIVE OBJECTS
+    this.signs = []
+    this.showingSign = false
+    this.map.filterObjects('Objects', (obj: any) => {
+      // DOORS
+      if (obj.name === 'door') {
+        ;(this.add as any).door(
+          Math.round(obj.x),
+          Math.round(obj.y),
+          obj.width,
+          obj.height,
+          obj.properties[0].value,
+          obj.properties[1].value
+        )
+        // last 2: destination (str) and link (bool, if true leads to a redirect)
+      }
+
+      // SIGNS
+      if (obj.name === 'sign') {
+        this.signs.push(
+          (this.add as any).sign(
+            obj.x,
+            obj.y,
+            obj.properties[1].value,
+            obj.properties[0].value
+          )
+        )
+        // Last parameters are the text to show and the direction of the text in relation to the object
+      }
+    })
+
+    this.events.on('break', this.catchDoBreak, this)
+    this.events.on('position', this.getPlayerPosition, this)
   }
 
   getPlayerPosition() {
@@ -54,7 +117,64 @@ export class BaseScene extends Phaser.Scene {
   }
 
   collide_with_world() {
+    // Collision with the world layers. Has to come after the rest of the colliders in order for them to detect.
+    // We need to call this at the end of the children's create
     this.physics.add.collider(this.player, this.LayerToCollide)
     this.LayerToCollide.setCollisionBetween(40, 41)
+
+    // Set the player to collide with the world bounds
+    this.player.body.setCollideWorldBounds(true)
+    this.player.body.onWorldBounds = true
+  }
+
+  // --------------------------------------------------------------------------------------------------
+  // UPDATE
+  update(time: any, delta: any) {
+    let moveleft = false
+    let moveright = false
+    let moveup = false
+    let movedown = false
+
+    // Not movable? stop movement and return
+    if (!this.movable) {
+      this.player.update(moveleft, moveright, moveup, movedown)
+      return false
+    }
+
+    // ----------------
+    // KEYBOARD
+    if (this.cursors.left.isDown || this.wasd.a.isDown) {
+      moveleft = true
+    } else if (this.cursors.right.isDown || this.wasd.d.isDown) {
+      moveright = true
+    }
+
+    if (this.cursors.up.isDown || this.wasd.w.isDown) {
+      moveup = true
+    } else if (this.cursors.down.isDown || this.wasd.s.isDown) {
+      movedown = true
+    }
+
+    this.player.update(moveleft, moveright, moveup, movedown)
+
+    // ---------------------
+    // INTERACTIVE OBJECTS
+    // Hide the signs
+    if (this.showingSign && (moveleft || moveright || moveup || movedown)) {
+      this.signs.forEach(
+        (sign: {
+          activated: any
+          playerMovement: (
+            arg0: boolean,
+            arg1: boolean,
+            arg2: boolean,
+            arg3: boolean
+          ) => void
+        }) => {
+          if (sign.activated)
+            sign.playerMovement(moveleft, moveright, moveup, movedown)
+        }
+      )
+    }
   }
 }
