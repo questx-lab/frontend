f01f991bc4c852e03a1de13ccf0bc6789edac33e
duyledat197 <duyledat197>
2023-05-05 22:03:24 +0700

    add remove cache

--- src/app/service/service.ts
+++ src/app/service/service.ts
@@ -1,5 +1,6 @@
 import { Rsp } from '@/types/common.type'
 
+// serialize
 export const serialize = (val: Rsp<any>): string => {
   return JSON.stringify(val)
 }
@@ -8,10 +9,19 @@ export const deserialize = (val: string | null): Rsp<any> => {
   return JSON.parse(val || '{}')
 }
 
+////// Cache /////
 export const loadCache = (val: string): string => {
   return sessionStorage.getItem(val) || ''
 }
 
 export const storeCache = (key: string, val: string) => {
   return sessionStorage.setItem(key, val)
+}
+
+export const removeCache = (key: string) => {
+  sessionStorage.removeItem(key)
+}
+
+export const clearCache = () => {
+  sessionStorage.clear()
 }
