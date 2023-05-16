304b2909bff4a0844132ed1d4346eb281a3b887b
Benja Heart <visopsys@users.noreply.github.com>
2023-05-16 16:16:38 -0700

    Add Horizontal & Vertical

---
+++ src/widgets/orientation.tsx
@@ -0,0 +1,14 @@
+import tw from 'twin.macro'
+
+export const Horizontal = tw.div`
+  flex
+  flex-row
+  justify-start
+  items-start
+`
+export const Vertical = tw.div`
+  flex
+  flex-col
+  justify-start
+  items-start
+`
