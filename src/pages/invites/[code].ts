8726c131c6357b2da4b61a5f26b411af24a352ef
duyledat197 <duyledat197>
2023-05-25 20:22:14 +0700

    fix to page

---
+++ src/pages/invites/[code].ts
@@ -0,0 +1,39 @@
+import type { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { getInviteApi } from '@/app/api/client/community'
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const code = context?.params?.code as string
+  try {
+    const resp = await getInviteApi(code)
+    if (resp.error) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: '/',
+        },
+      }
+    }
+
+    const communityId = resp.data?.community.id
+    const userId = resp.data?.user.id
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/communities/projects/${communityId}?invited_by=${userId}`,
+      },
+    }
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    }
+  }
+}
+
+export default function Page({
+  repo,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return repo.stargazers_count
+}
