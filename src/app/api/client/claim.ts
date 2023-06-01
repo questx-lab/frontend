4d702bff64af2243dc0e5b1490e610b2a0f7726a
quandat10 <datcckkdat@gmail.com>
2023-06-01 22:38:34 +0700

    change

--- src/app/api/client/claim.ts
+++ src/app/api/client/claim.ts
@@ -1,7 +1,7 @@
 import { api } from '@/app/api/config/api'
 import { ClaimedQuestStatus } from '@/constants/common.const'
 import { EnvVariables } from '@/constants/env.const'
-import { ListClaimQuestType, ReqClaimReward, Rsp } from '@/utils/type'
+import { ListClaimQuestType, Rsp } from '@/utils/type'
 
 export const listClaimedQuestsApi = async (
   id: string,
@@ -23,12 +23,5 @@ export const updateClaimedQuestApi = async (ids: string[], action: string): Prom
     ids,
     action,
   })
-  return data
-}
-
-export const claimRewardApi = async (
-  body: ReqClaimReward
-): Promise<Rsp<{ id: string; status: string }>> => {
-  const { data } = await api.post(EnvVariables.NEXT_PUBLIC_API_URL + '/claim', body)
   return data
 }
