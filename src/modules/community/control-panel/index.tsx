937941f684589c2cd9df580e8d0428fee442c593
Benja Heart <visopsys@users.noreply.github.com>
2023-06-02 16:21:56 -0700

    Refactor control panel

---
+++ src/modules/community/control-panel/index.tsx
@@ -0,0 +1,73 @@
+import { FunctionComponent } from 'react'
+
+import tw from 'twin.macro'
+
+import { StorageConst } from '@/constants/storage.const'
+import QuestsTab from '@/modules/community/control-panel/quests'
+import ReviewSubmissionsTab from '@/modules/community/control-panel/review-submissions'
+import SettingsTab from '@/modules/community/control-panel/settings'
+import { Divider, Gap } from '@/styles/common.style'
+import { CommunityType } from '@/utils/type'
+import { CircularImage } from '@/widgets/circular-image'
+import { HorizontalCenter, VerticalCenter } from '@/widgets/orientation'
+
+export const FixedFrame = tw.div`
+  w-80
+  fixed
+  border-r-2
+  border-gray-200
+  h-full
+`
+
+export const PersonWrap = tw(VerticalCenter)`
+  p-6
+`
+
+const TextName = tw.p`
+  text-lg
+  font-medium
+  text-black
+  text-center
+  max-w-lg
+  text-ellipsis
+  overflow-hidden
+  max-w-[150px]
+`
+
+const Padding = tw.div`
+  px-4
+`
+
+export const ControlPanel: FunctionComponent<{
+  community: CommunityType
+  show: boolean
+}> = ({ community, show }) => {
+  if (!show) {
+    return <></>
+  }
+
+  return (
+    <FixedFrame>
+      <PersonWrap>
+        <CircularImage
+          width={80}
+          height={80}
+          src={community.logo_url || StorageConst.COMMUNITY_DEFAULT.src}
+          alt={StorageConst.COMMUNITY_DEFAULT.alt}
+        />
+        <Gap height={6} />
+
+        <HorizontalCenter>
+          <TextName>{community.display_name}</TextName>
+          <Gap width={1} />
+        </HorizontalCenter>
+      </PersonWrap>
+      <Divider />
+      <Padding>
+        <QuestsTab />
+        <ReviewSubmissionsTab />
+        <SettingsTab />
+      </Padding>
+    </FixedFrame>
+  )
+}
---
+++ src/modules/community/control-panel/review-submissions.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import { Tab } from '@/modules/community/control-panel/mini-widgets'
+import { CommunityStore } from '@/store/local/community'
+import { ControlPanelTab } from '@/types/community'
+import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline'
+
+const ReviewSubmissionsTab: FC = () => {
+  // data
+  const activeControlPanelTab = CommunityStore.useStoreState((state) => state.activeControlPanelTab)
+
+  // hook
+  const navigate = useNavigate()
+
+  return (
+    <Tab
+      onClick={() => {
+        if (activeControlPanelTab === ControlPanelTab.REVIEW_SUBMISSION) {
+          return
+        }
+        navigate('./review')
+      }}
+      active={activeControlPanelTab === ControlPanelTab.REVIEW_SUBMISSION}
+    >
+      <ArrowUpOnSquareIcon className='w-6 h-6 mr-2' />
+      {'REVIEW SUBMISSION'}
+    </Tab>
+  )
+}
+
+export default ReviewSubmissionsTab
