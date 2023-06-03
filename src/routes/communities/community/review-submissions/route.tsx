e6ef5961a6a695713955382131c24ae4623fae54
Benja Heart <visopsys@users.noreply.github.com>
2023-06-03 16:41:43 -0700

    Add router protection for review submission and settings

--- src/routes/communities/community/review-submissions/route.tsx
+++ src/routes/communities/community/review-submissions/route.tsx
@@ -1,19 +1,35 @@
 import { FunctionComponent, useEffect } from 'react'
 
-import { Outlet } from 'react-router-dom'
+import { useStoreState } from 'easy-peasy'
+import { Outlet, useNavigate } from 'react-router-dom'
 
+import { CommunityRoleEnum } from '@/constants/common.const'
+import { RouterConst } from '@/constants/router.const'
 import { NewClaimReviewStore } from '@/store/local/claim-review'
 import { CommunityStore } from '@/store/local/community'
+import { GlobalStoreModel } from '@/store/store'
 import { ControlPanelTab } from '@/types/community'
 
 export const ReviewSubmissions: FunctionComponent = () => {
+  // data
+  const user = useStoreState<GlobalStoreModel>((state) => state.user)
+  const role = CommunityStore.useStoreState((action) => action.role)
+  const isOwner = role === CommunityRoleEnum.OWNER
+
+  // action
   const setActiveControlPanelTab = CommunityStore.useStoreActions(
     (action) => action.setActiveControlPanelTab
   )
 
+  const navigate = useNavigate()
   useEffect(() => {
+    if (!user || !isOwner) {
+      navigate(RouterConst.COMMUNITIES)
+      return
+    }
+
     setActiveControlPanelTab(ControlPanelTab.REVIEW_SUBMISSION)
-  }, [setActiveControlPanelTab])
+  }, [setActiveControlPanelTab, user, isOwner, navigate])
 
   return (
     <NewClaimReviewStore.Provider>
--- src/routes/communities/community/settings/route.tsx
+++ src/routes/communities/community/settings/route.tsx
@@ -1,10 +1,36 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect } from 'react'
 
-import { Outlet } from 'react-router-dom'
+import { useStoreState } from 'easy-peasy'
+import { Outlet, useNavigate } from 'react-router-dom'
 
+import { CommunityRoleEnum } from '@/constants/common.const'
+import { RouterConst } from '@/constants/router.const'
+import { CommunityStore } from '@/store/local/community'
 import NewCommunityStore from '@/store/local/new-community.store'
+import { GlobalStoreModel } from '@/store/store'
+import { ControlPanelTab } from '@/types/community'
 
 export const Settings: FunctionComponent = () => {
+  // data
+  const user = useStoreState<GlobalStoreModel>((state) => state.user)
+  const role = CommunityStore.useStoreState((action) => action.role)
+  const isOwner = role === CommunityRoleEnum.OWNER
+
+  // action
+  const setActiveControlPanelTab = CommunityStore.useStoreActions(
+    (action) => action.setActiveControlPanelTab
+  )
+
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!user || !isOwner) {
+      navigate(RouterConst.COMMUNITIES)
+      return
+    }
+
+    setActiveControlPanelTab(ControlPanelTab.SETTINGS)
+  }, [setActiveControlPanelTab, user, isOwner, navigate])
+
   return (
     <NewCommunityStore.Provider>
       <Outlet />
