33dccb582e9955d47c097d816bfe8d80185e5c1f
quandat10 <datcckkdat@gmail.com>
2023-06-06 13:51:51 +0700

    fix

--- src/widgets/modal/loading.tsx
+++ src/widgets/modal/loading.tsx
@@ -10,48 +10,51 @@ import { CheckBadgeIcon, ExclamationTriangleIcon } from '@heroicons/react/24/out
 import { VerticalFullWidthCenter } from '../orientation'
 import { NormalText } from '../text'
 
+const RenderContent: FunctionComponent<{ isSuccess?: boolean; message?: string }> = ({
+  isSuccess,
+  message,
+}) => {
+  if (isSuccess === false) {
+    return (
+      <VerticalFullWidthCenter>
+        <ExclamationTriangleIcon className='w-15 h-15 text-danger' />
+        <NormalText>{message}</NormalText>
+      </VerticalFullWidthCenter>
+    )
+  }
+
+  if (isSuccess === true) {
+    return (
+      <VerticalFullWidthCenter>
+        <CheckBadgeIcon className='w-15 h-15 text-success' />
+        <NormalText>{message}</NormalText>
+      </VerticalFullWidthCenter>
+    )
+  }
+
+  return (
+    <MoonLoader
+      color={'#000'}
+      loading={true}
+      cssOverride={SpinnerStyle}
+      size={50}
+      aria-label='Loading Spinner'
+      data-testid='loader'
+    />
+  )
+}
+
 const LoadingModal: FunctionComponent<{
   isOpen?: boolean
   isSuccess?: boolean
   message?: string
 }> = ({ isOpen = false, isSuccess, message }) => {
-  const RenderContent: FunctionComponent = () => {
-    if (isSuccess === false) {
-      return (
-        <VerticalFullWidthCenter>
-          <ExclamationTriangleIcon className='w-15 h-15 text-danger' />
-          <NormalText>{message}</NormalText>
-        </VerticalFullWidthCenter>
-      )
-    }
-
-    if (isSuccess === true) {
-      return (
-        <VerticalFullWidthCenter>
-          <CheckBadgeIcon className='w-15 h-15 text-success' />
-          <NormalText>{message}</NormalText>
-        </VerticalFullWidthCenter>
-      )
-    }
-
-    return (
-      <MoonLoader
-        color={'#000'}
-        loading={true}
-        cssOverride={SpinnerStyle}
-        size={50}
-        aria-label='Loading Spinner'
-        data-testid='loader'
-      />
-    )
-  }
-
   return (
     <BaseModal isOpen={isOpen}>
       <CenterWrap>
         <DialogPannel>
           <WrapProgressBar>
-            <RenderContent />
+            <RenderContent isSuccess={isSuccess} message={message} />
           </WrapProgressBar>
         </DialogPannel>
       </CenterWrap>
