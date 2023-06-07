c5dbe80649d5430c8dfada017ab95378a2c035c9
Benja Heart <510036+visopsys@users.noreply.github.com>
2023-06-06 17:26:37 -0700

    bugfix: allow user to choose different answer when they choose the wrong one (#120)
    * Only allow guest to claim quizzes
    
    * Show next button when user chooses correct answer
    
    ---------
    
    Co-authored-by: Benja Heart <visopsys@users.noreply.github.com>

--- src/modules/quest/view-quest/quiz.tsx
+++ src/modules/quest/view-quest/quiz.tsx
@@ -1,19 +1,15 @@
-import { FunctionComponent, useEffect, useState } from 'react'
+import { FC, FunctionComponent, useEffect, useState } from 'react'
 
 import tw from 'twin.macro'
 
 import { Alphabet } from '@/constants/alphabet'
+import { AnswerStatusEnum } from '@/constants/common.const'
 import ActiveQuestStore from '@/store/local/active-quest'
 import { QuestQuizType } from '@/types/quest'
+import { PositiveButton } from '@/widgets/buttons'
 import { AnswerBox } from '@/widgets/buttons/answer-box'
-import {
-  HorizontalCenter,
-  HorizontalStartCenter,
-  Vertical,
-  VerticalFullWidth,
-} from '@/widgets/orientation'
-import { Label, NormalText } from '@/widgets/text'
-import { CheckCircleIcon } from '@heroicons/react/24/outline'
+import { HorizontalStartCenter, Vertical, VerticalFullWidth } from '@/widgets/orientation'
+import { Label, NormalText, SmallText } from '@/widgets/text'
 
 const QuizBox = tw(Vertical)`
   w-full
@@ -53,41 +49,113 @@ const SquareBox = tw.div`
   items-center
 `
 
+const NextButtonStyle = tw.div`
+  w-full
+  flex
+  items-end
+  justify-end
+`
+
+const ErrorText = tw(SmallText)`
+  text-danger-500
+`
+
+const ErrorMessage: FC<{ userAnswer: string; correct: boolean }> = ({ userAnswer, correct }) => {
+  if (userAnswer === '' || correct) {
+    return <></>
+  }
+
+  return <ErrorText>Please try again</ErrorText>
+}
+
+const NextButton: FC<{
+  show: boolean
+  enable: boolean
+  onClick: () => void
+}> = ({ show, enable, onClick }) => {
+  if (!show) {
+    return <></>
+  }
+
+  return (
+    <NextButtonStyle>
+      <PositiveButton block={!enable} onClick={onClick}>
+        Next
+      </PositiveButton>
+    </NextButtonStyle>
+  )
+}
+
 export const QuizItem: FunctionComponent<{
   quiz: QuestQuizType
   quizNum: number
+  canClaim: boolean
+  isLastQuiz: boolean
   setQuizNum: (e: number) => void
-}> = ({ quiz, setQuizNum, quizNum }) => {
+}> = ({ quiz, quizNum, canClaim, isLastQuiz, setQuizNum }) => {
   // store
   const quizAnswers = ActiveQuestStore.useStoreState((state) => state.quizAnswers)
   const setQuizAnswers = ActiveQuestStore.useStoreActions((action) => action.setQuizAnswers)
 
+  // local state
+  const userAnswer = quizAnswers.length > quizNum ? quizAnswers[quizNum] : ''
+  const correct = userAnswer !== '' && userAnswer === quiz.answers[0]
+
   // handler
-  const onChange = (choose: string) => {
-    setQuizAnswers([...quizAnswers, choose])
+  const onChange = (selectedAnswer: string) => {
+    // Only enable the answer if user is a guest. Don't accept answer if this is an admin or not
+    // logged in.
+    if (canClaim) {
+      if (quizAnswers.length > quizNum) {
+        quizAnswers[quizNum] = selectedAnswer
+      } else {
+        quizAnswers.push(selectedAnswer)
+      }
+
+      setQuizAnswers([...quizAnswers])
+    }
+  }
+
+  const onNextClicked = () => {
     setQuizNum(quizNum + 1)
   }
 
-  const renderAnsers =
-    quiz &&
-    quiz.options.map((e, i) => (
-      <AnswerBox key={i} onClick={() => onChange(e)}>
+  const renderAnswers = quiz.options.map((option, i) => {
+    let status = AnswerStatusEnum.DEFAULT
+    if (userAnswer === option) {
+      if (correct) {
+        // correct answer
+        status = AnswerStatusEnum.ACTIVE
+      } else {
+        // incorrect answer
+        status = AnswerStatusEnum.DANGER
+      }
+    }
+
+    return (
+      <AnswerBox key={i} onClick={() => onChange(option)} status={status}>
         <SquareBox>{Alphabet[i]}</SquareBox>
-        <AnswerText>{e}</AnswerText>
+        <AnswerText>{option}</AnswerText>
       </AnswerBox>
-    ))
+    )
+  })
 
   return (
     <ContentBox>
       <Label>{'Question ❓'}</Label>
       <NormalText>{quiz.question}</NormalText>
       <Label>{'SUBMISSION 📝'}</Label>
-      {renderAnsers}
+      {renderAnswers}
+      <ErrorMessage userAnswer={userAnswer} correct={correct} />
+      <NextButton show={!isLastQuiz} enable={correct} onClick={onNextClicked} />
     </ContentBox>
   )
 }
 
-export const QuestQuiz: FunctionComponent<{ quizzes: QuestQuizType[] }> = ({ quizzes }) => {
+export const QuestQuiz: FunctionComponent<{ quizzes: QuestQuizType[]; canClaim: boolean }> = ({
+  quizzes,
+  canClaim,
+}) => {
   const [quizNum, setQuizNum] = useState<number>(0)
   const setQuizAnswers = ActiveQuestStore.useStoreActions((action) => action.setQuizAnswers)
 
@@ -95,20 +163,17 @@ export const QuestQuiz: FunctionComponent<{ quizzes: QuestQuizType[] }> = ({ qui
     setQuizAnswers([])
   }, [])
 
-  if (quizNum === quizzes.length) {
-    return (
-      <HorizontalCenter>
-        <CheckCircleIcon className='w-10 h-10 text-success' />
-        {'You could submit your answer'}
-      </HorizontalCenter>
-    )
-  }
-
   return (
     <QuizBox>
       <HeaderQuizBox>
         <Label>{`${quizNum + 1} of ${quizzes.length} quests completed`}</Label>
-        <QuizItem quiz={quizzes[quizNum]} quizNum={quizNum} setQuizNum={setQuizNum} />
+        <QuizItem
+          quiz={quizzes[quizNum]}
+          quizNum={quizNum}
+          setQuizNum={setQuizNum}
+          canClaim={canClaim}
+          isLastQuiz={quizNum === quizzes.length - 1}
+        />
       </HeaderQuizBox>
     </QuizBox>
   )
