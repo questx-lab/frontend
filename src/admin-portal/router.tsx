95e02a8aff535209ccfa0d3569c56a10bf5c2511
Benja Heart <visopsys@users.noreply.github.com>
2023-06-07 22:24:56 -0700

    WIP

--- src/admin-portal/router.tsx
+++ src/admin-portal/router.tsx
@@ -1,3 +1,11 @@
-const router = [{}]
+import Root, { RootLoader } from '@/admin-portal/routes/route'
+
+const router = [
+  {
+    path: '/',
+    element: <Root />,
+    loader: RootLoader,
+  },
+]
 
 export default router
