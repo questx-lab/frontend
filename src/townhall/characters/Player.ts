5ae8141fa84dc5c5bb8e2e5b218694a82b8a2187
quandat10 <datcckkdat@gmail.com>
2023-06-14 23:08:09 +0700

    refactor

--- src/townhall/characters/Player.ts
+++ src/townhall/characters/Player.ts
@@ -69,48 +69,4 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
       .setSize(this.width * collisionScale[0], this.height * collisionScale[1])
       .setOffset(-8, this.height * (1 - collisionScale[1]) + 6)
   }
-
-  updateDialogBubble(content: string) {
-    this.clearDialogBubble()
-
-    // preprocessing for dialog bubble text (maximum 70 characters)
-    const dialogBubbleText = content.length <= 70 ? content : content.substring(0, 70).concat('...')
-
-    const innerText = this.scene.add
-      .text(0, 0, dialogBubbleText, { wordWrap: { width: 165, useAdvancedWrap: true } })
-      .setFontFamily('Arial')
-      .setFontSize(12)
-      .setColor('#000000')
-      .setOrigin(0.5)
-
-    // set dialogBox slightly larger than the text in it
-    const innerTextHeight = innerText.height
-    const innerTextWidth = innerText.width
-
-    innerText.setY(-innerTextHeight / 2 - this.playerName.height / 2)
-    const dialogBoxWidth = innerTextWidth + 10
-    const dialogBoxHeight = innerTextHeight + 3
-    const dialogBoxX = innerText.x - innerTextWidth / 2 - 5
-    const dialogBoxY = innerText.y - innerTextHeight / 2 - 2
-
-    this.playerDialogBubble.add(
-      this.scene.add
-        .graphics()
-        .fillStyle(0xffffff, 1)
-        .fillRoundedRect(dialogBoxX, dialogBoxY, dialogBoxWidth, dialogBoxHeight, 3)
-        .lineStyle(1, 0x000000, 1)
-        .strokeRoundedRect(dialogBoxX, dialogBoxY, dialogBoxWidth, dialogBoxHeight, 3)
-    )
-    this.playerDialogBubble.add(innerText)
-
-    // After 6 seconds, clear the dialog bubble
-    this.timeoutID = window.setTimeout(() => {
-      this.clearDialogBubble()
-    }, 6000)
-  }
-
-  private clearDialogBubble() {
-    clearTimeout(this.timeoutID)
-    this.playerDialogBubble.removeAll(true)
-  }
 }
