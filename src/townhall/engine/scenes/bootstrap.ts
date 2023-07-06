33994828183fd687b6f8aec9407a8cb383589806
Benja Heart <visopsys@users.noreply.github.com>
2023-07-06 14:44:38 -0700

    Remove loading unused assets

--- src/townhall/engine/scenes/bootstrap.ts
+++ src/townhall/engine/scenes/bootstrap.ts
@@ -25,12 +25,6 @@ export default class Bootstrap extends Phaser.Scene {
     // TODO: lazy loading
     this.load.baseURL = EnvVariables.TOWNHALL_ASSET_CDN
 
-    this.load.atlas('cloud_day', '/background/cloud_day.png', '/background/cloud_day.json')
-    this.load.image('backdrop_day', '/background/backdrop_day.png')
-    this.load.atlas('cloud_night', '/background/cloud_night.png', '/background/cloud_night.json')
-    this.load.image('backdrop_night', '/background/backdrop_night.png')
-    this.load.image('sun_moon', '/background/sun_moon.png')
-
     this.load.tilemapTiledJSON('tilemap', '/map/jp_map.json')
     this.load.spritesheet('tiles_wall', '/map/jp_tileset.png', {
       frameWidth: 32,
