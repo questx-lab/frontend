9ba900ff8629a65e7e0df3d899476d616a47ab5a
quandat10 <datcckkdat@gmail.com>
2023-07-18 01:44:33 +0700

    fix unauthor

--- src/platform/routes/messages/routes.tsx
+++ src/platform/routes/messages/routes.tsx
@@ -1,8 +1,19 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+
+import { RouterConst } from '@/constants/router.const'
+import { isLogin } from '@/utils/helper'
 
 const Messages: FC = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLogin()) {
+      navigate(RouterConst.COMMUNITIES)
+    }
+  }, [])
+
   return <Outlet />
 }
 
