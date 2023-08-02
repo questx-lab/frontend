e1e550cfc475bd5facd8b024afbbe82cd59e42dc
Benja Heart <visopsys@users.noreply.github.com>
2023-08-02 16:11:30 -0700

    Use enum instead of string and number

--- src/modules/header/user-profile/rewards-badges.tsx
+++ src/modules/header/user-profile/rewards-badges.tsx
@@ -1,15 +1,20 @@
-import { FC, useEffect, useState } from 'react'
-import styled from 'styled-components'
+import { FC, useState } from 'react'
+
 import { useStoreState } from 'easy-peasy'
 import tw from 'twin.macro'
-import { BadgeType } from '@/types'
+
 import ClaimHistory from '@/modules/header/user-profile/claim-history'
 import { GlobalStoreModel } from '@/store/store'
 import { UserType } from '@/types'
 import { Vertical } from '@/widgets/orientation'
-import { Tab, TabItem } from '@/widgets/tab-group/focus-light-primary'
 import { Gap } from '@/widgets/separator'
-import { toast } from 'react-hot-toast'
+import { Tab, TabItem } from '@/widgets/tab-group/focus-light-primary'
+
+enum TabType {
+  HISTORY = 'HISTORY',
+}
+
+const tabList = [TabType.HISTORY]
 
 const BoundingBox = tw(Vertical)`
   w-2/3
@@ -18,45 +23,19 @@ const BoundingBox = tw(Vertical)`
   max-sm:w-full
 `
 
-const CounterBox = styled.div<{ active: boolean }>(({ active }) => {
-  const style = [
-    tw`
-      rounded-full
-      border-2
-      px-2
-      py-0
-      `,
-  ]
-
-  if (active) {
-    style.push(tw`
-        bg-primary-500
-        text-white
-      `)
-  } else {
-    style.push(tw`
-        bg-[#E5E7EB]
-        text-black
-      `)
-  }
-
-  return style
-})
-
-const tabList = ['HISTORY']
-const ControlTabs: FC<{ activePos: number; setActivePos: (activePos: number) => void }> = ({
-  activePos,
-  setActivePos,
+const ControlTabs: FC<{ activeTab: TabType; setActiveTab: (activeTab: TabType) => void }> = ({
+  activeTab,
+  setActiveTab,
 }) => {
   return (
     <Tab>
       {tabList.map((tab, idx) => (
         <>
           <TabItem
-            active={idx === activePos}
+            active={tab === activeTab}
             tabCount={tabList.length}
             position={idx}
-            onClick={() => setActivePos(idx)}
+            onClick={() => setActiveTab(tab)}
           >
             {tab}
           </TabItem>
@@ -69,15 +48,15 @@ const ControlTabs: FC<{ activePos: number; setActivePos: (activePos: number) => 
 const RewardsBadges: FC<{ user: UserType }> = ({ user }) => {
   // TODO: Add badges and ranking
   const me: UserType = useStoreState<GlobalStoreModel>((state) => state.user)
-  const [activePos, setActivePos] = useState<number>(0)
+  const [activeTab, setActiveTab] = useState<TabType>(TabType.HISTORY)
 
   if (user.id !== me.id) {
     return <></>
   }
 
   const Panel = () => {
-    switch (activePos) {
-      case 0:
+    switch (activeTab) {
+      case TabType.HISTORY:
         return <ClaimHistory user={user} />
 
       default:
@@ -87,7 +66,7 @@ const RewardsBadges: FC<{ user: UserType }> = ({ user }) => {
 
   return (
     <BoundingBox>
-      <ControlTabs activePos={activePos} setActivePos={setActivePos} />
+      <ControlTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       <Gap height={5} />
       <Panel />
     </BoundingBox>
