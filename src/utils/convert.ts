787f376ea321ea3c84fc7e2e3ed065b936adcb02
duyledat197 <38448882+duyledat197@users.noreply.github.com>
2023-08-04 00:46:35 +0700

    feat: mention in chat (#296)
    * add yarn
    
    * add mention UI
    
    * clean
    
    * add react mentions
    
    * fix list
    
    * add debounce
    
    * add limit
    
    * fix style name
    
    * change state name
    
    ---------
    
    Co-authored-by: duyledat197 <duyledat197>

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -38,3 +38,13 @@ export const encode = (s: string) => {
   const buffer = Buffer.from(compress)
   return buffer.toString('base64')
 }
+
+export const getInfoFromMarkup = (markup: string) => {
+  return [...markup.matchAll(/@\[(.*?)]\((.*?)\)/g)].map((list: string[]) => {
+    return {
+      reg: list[0],
+      display: list[1],
+      id: list[2],
+    }
+  })
+}
