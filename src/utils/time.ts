1e135988eef2831f130f19b9ad9f3f892d204a09
Đạt <43912089+quandat10@users.noreply.github.com>
2023-08-09 12:43:03 +0700

    fix:show quest with countdown time [XQUEST-33] (#327)
    * fix:show quest with countdown time
    
    * fix remaining day
    
    * fix block questcard

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -23,6 +23,22 @@ export const calculateTimeRemaining = (targetDate: Date) => {
   return { hours, minutes, seconds }
 }
 
+export const calculateDayRemaining = (targetDate: Date) => {
+  const difference = targetDate.getTime() - new Date().getTime()
+
+  if (difference <= 0) {
+    // target date has passed, stop the countdown
+    return { days: '00', hours: '00', minutes: '00', seconds: '00' }
+  }
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24)).toString()
+  const hours = padNumberWithZero(Math.floor((difference / (1000 * 60 * 60)) % 24))
+  const minutes = padNumberWithZero(Math.floor((difference / 1000 / 60) % 60))
+  const seconds = padNumberWithZero(Math.floor((difference / 1000) % 60))
+
+  return { days, hours, minutes, seconds }
+}
+
 export const padNumberWithZero = (num: number, length: number = 2) => {
   return num.toString().padStart(length, '0')
 }
