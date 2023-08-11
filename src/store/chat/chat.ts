2142b2e3fe8b4d87c32e33133f407d988404f07d
duyledat197 <38448882+duyledat197@users.noreply.github.com>
2023-08-11 07:28:23 +0700

    feat: add red dot message box [XQUEST-51] (#336)
    * add red dot message box
    
    * add fullmode
    
    * fix logic
    
    * Remove duplicated listener
    
    ---------
    
    Co-authored-by: duyledat197 <duyledat197>
    Co-authored-by: Benja Heart <visopsys@users.noreply.github.com>

--- src/store/chat/chat.ts
+++ src/store/chat/chat.ts
@@ -1,21 +1,25 @@
 import { action, Action, createContextStore } from 'easy-peasy'
 
-import { ChannelType, emptyChannel, TabChatType } from '@/types/chat'
+import { ChannelType, emptyChannel, NewMessageStatusType, TabChatType } from '@/types/chat'
+import { EqualBigInt } from '@/utils/number'
 
 interface ChatModel {
   selectedTab: TabChatType
   selectedChannel: ChannelType
   selectedAssetsUri: string
+  channelNewMessageStatus: NewMessageStatusType[]
 
   setTab: Action<ChatModel, TabChatType>
   setChannel: Action<ChatModel, ChannelType>
   setSelectedAssetsUri: Action<ChatModel, string>
+  setChannelNewMessageStatus: Action<ChatModel, NewMessageStatusType>
 }
 
 const ChatStore = createContextStore<ChatModel>({
   selectedTab: TabChatType.Chat,
   selectedChannel: emptyChannel(),
   selectedAssetsUri: '',
+  channelNewMessageStatus: [],
 
   setTab: action((state, newTab) => {
     state.selectedTab = newTab
@@ -27,6 +31,20 @@ const ChatStore = createContextStore<ChatModel>({
 
   setSelectedAssetsUri: action((state, uri) => {
     state.selectedAssetsUri = uri
+  }),
+
+  setChannelNewMessageStatus: action((state, newMessageStatus) => {
+    let exist = state.channelNewMessageStatus.find((status) =>
+      EqualBigInt(status.channelId, newMessageStatus.channelId)
+    )
+    if (exist)
+      state.channelNewMessageStatus = state.channelNewMessageStatus.map((status) => {
+        if (EqualBigInt(status.channelId, newMessageStatus.channelId)) return newMessageStatus
+        else return status
+      })
+    else {
+      state.channelNewMessageStatus = [...state.channelNewMessageStatus, newMessageStatus]
+    }
   }),
 })
 
